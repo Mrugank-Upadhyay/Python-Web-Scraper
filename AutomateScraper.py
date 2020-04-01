7bccf8d23e0d6c5a624cf9c6f1ae4dc9e71c1ea3
Mrugank <mrugank2@gmail.com>
2020-04-01 03:44:24 -0400

    added to automation file

--- AutomateScraper.py
+++ AutomateScraper.py
@@ -9,6 +9,15 @@ page = requests.get(page_input)
 page_content = BeautifulSoup(page.content, 'html.parser')
 
 main_tag_input = input("Main Tag: ")
-main_tag = page_content.find(main_tag_input)
+if ("[id] " in main_tag_input):
+    main_tag = page_content.find(id= ''.join(str(elem) for elem in list(main_tag_input)[5::]))
+else:
+    main_tag = page_content.find(class_= main_tag_input)
 
 tags = []
+
+tag_input = ""
+
+while (tag_input != "quit"):
+    tag_input = input("Enter Tag: ")
+    
